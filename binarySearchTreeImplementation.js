c1e0753692a40a5262490276cd74fccb6a980239
phillipashe <ashephillip@gmail.com>
2022-05-28 09:56:19 -0400

    initial commit

---
+++ binarySearchTreeImplementation.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(val) {
+      this.left = null;
+      this.right = null;
+      this.val = val;
+    }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
