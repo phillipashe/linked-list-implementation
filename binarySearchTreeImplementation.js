d92b5db1cfc909eb7417a1ddf3ea8424ea7a4dd2
phillipashe <ashephillip@gmail.com>
2022-05-28 10:34:38 -0400

    Implemented insert

--- binarySearchTreeImplementation.js
+++ binarySearchTreeImplementation.js
@@ -1,13 +1,48 @@
 class Node {
-    constructor(val) {
-      this.left = null;
-      this.right = null;
-      this.val = val;
-    }
+  constructor(val) {
+    this.left = null;
+    this.right = null;
+    this.val = val;
+  }
 }
 
 class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  /* 
+    Will implement the following: 
+    x insert(data)
+    remove(data)
+               
+ 
+    Helper functions:
+     findMinNode()
+     getRootNode()
+     inorder(node)
+     preorder(node)              
+     postorder(node)
+     search(node, data)
+  */
+
+
+  insertData(val) {
+    const newNode = new Node(val);
+    if (!this.root) return this.root = newNode;
+
+    this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.val < node.val) {
+      if (!node.left) return node.left = newNode;
+      return this.insertNode(node.left, newNode);
+    }
+    if (newNode.val >= node.val) {
+      if (!node.right) return node.right = newNode;
+      return this.insertNode(node.right, newNode);
+    }
+  }
 }
+
+module.exports = { BinarySearchTree }
--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const { Root, BinarySearchTree } = require('./binarySearchTreeImplementation');
+const { BinarySearchTree } = require('./binarySearchTreeImplementation');
+const nodeValues = require('./nodeValues.json');
+
+const tree = new BinarySearchTree();
+
+// build tree
+for (val of nodeValues) tree.insertData(val); 
+console.log(tree);
